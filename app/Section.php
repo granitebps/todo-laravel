a6ac6506c221f67680095f1ffcaf80db1a0d8d75
Granite Bagas <granitebagas28@gmail.com>
2020-09-11 20:29:56 +0700

    :construction: Add relation to task in section model

--- app/Section.php
+++ app/Section.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     protected $guarded = [];
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');
+    }
 }
