bf9a0671e9b053229249e93aeebd732c64046b86
Granite Bagas <granitebagas28@gmail.com>
2020-09-11 21:51:02 +0700

    :construction: Change created_at task format

--- app/Task.php
+++ app/Task.php
@@ -2,11 +2,17 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     protected $guarded = [];
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->diffForHumans();
+    }
 
     public function section()
     {
