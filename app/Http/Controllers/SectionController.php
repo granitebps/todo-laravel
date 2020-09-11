a348b2352ee93b1e643370c375943a27424f4402
Granite Bagas <granitebagas28@gmail.com>
2020-09-11 20:45:17 +0700

    :sparkles: Add route and function to create a section

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -5,13 +5,32 @@ namespace App\Http\Controllers;
 use App\Section;
 use App\Traits\Helpers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SectionController extends Controller
 {
     public function index()
     {
-        $sections = Section::with('task')->latest()->get();
+        $sections = Section::with('tasks')->latest()->get();
 
         return Helpers::apiResponse(true, '', $sections);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+
+        DB::beginTransaction();
+        try {
+            Section::create($data);
+            DB::commit();
+
+            return Helpers::apiResponse(true, 'Section Created', [], 201);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return Helpers::apiResponse(false, $th->getMessage(), [], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::get('sections', 'SectionController@index');
+    Route::post('sections', 'SectionController@store');
 });
